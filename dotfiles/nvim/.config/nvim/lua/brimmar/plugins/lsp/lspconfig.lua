4c26dd354f74005d26da46c9b9e440b6e7e14343
Bryan Martin <50660327+brimmar@users.noreply.github.com>
2024-04-04 06:26:30 -0300

    feat(neovim): add go lsp to lsp config in neovim (#27)

--- dotfiles/nvim/.config/nvim/lua/brimmar/plugins/lsp/lspconfig.lua
+++ dotfiles/nvim/.config/nvim/lua/brimmar/plugins/lsp/lspconfig.lua
@@ -181,5 +181,10 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		lspconfig["gopls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
