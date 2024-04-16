4e8746299e6923da0b7220f427f05252d2475c4f
Bryan <bmnbfs7@gmail.com>
2024-04-16 08:36:10 -0300

    feat(neovim): switch php cs fixer formatter to laravel pint formatter

--- dotfiles/nvim/.config/nvim/lua/brimmar/plugins/formatting.lua
+++ dotfiles/nvim/.config/nvim/lua/brimmar/plugins/formatting.lua
@@ -1,36 +1,36 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function ()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettierd" },
-                typescript = { "prettierd" },
-                svelte = { "prettierd" },
-                css = { "prettierd" },
-                html = { "prettierd" },
-                json = { "prettierd" },
-                yaml = { "prettierd" },
-                markdown = { "prettierd" },
-                lua = { "stylua" },
-                blade = { "blade-formatter" },
-                php = { "php-cs-fixer" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500
-            }
-        })
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				svelte = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				lua = { "stylua" },
+				blade = { "blade-formatter" },
+				php = { "pint" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
 
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function ()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500
-            })
-        end, {desc = "Format file or range (in visual or normal mode)"})
-    end
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual or normal mode)" })
+	end,
 }
